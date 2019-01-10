ace2f7e0f9d08672dc7bb91695d89c6f22223eb7
abuts <Alex.Buts@stfc.ac.uk>
2019-01-10 20:14:48 +0000

    deleted resolution_function folder as moved it into separate repository

--- Resolution_functions/g_filter.m
+++
@@ -1,9 +0,0 @@
-function s_int = g_filter(omega,s_int,sigma)
-% function applies Gaussian filter to the input spectrum
-%
-omega_max = max(omega);
-g_f = exp(-(omega/(omega_max*sigma)).^2);
-Norm = sum(g_f);
-g_f  = g_f/Norm;
-s_int = s_int.*g_f;
-
